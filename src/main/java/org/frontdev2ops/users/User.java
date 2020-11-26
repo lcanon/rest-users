b709c11456f58e6e87e380023a0726f08b68f686
Javier Fernández Pérez <javier.fernandez2@datiobbva.com>
2020-11-26 20:48:32 +0100

    Added health check

--- src/main/java/org/frontdev2ops/users/User.java
+++ src/main/java/org/frontdev2ops/users/User.java
@@ -2,24 +2,17 @@
 package org.frontdev2ops.users;
 
 import io.quarkus.hibernate.orm.panache.PanacheEntity;
-import io.quarkus.security.jpa.Password;
-import io.quarkus.security.jpa.Roles;
-import io.quarkus.security.jpa.UserDefinition;
-import io.quarkus.security.jpa.Username;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 
 @Entity
 @Table(name = "users")
-@UserDefinition
 public class User extends PanacheEntity {
 
-    @Username
     @NotNull
     public String username;
 
-    @Password
     @NotNull
     public String password;
 
